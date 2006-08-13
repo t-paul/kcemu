e03dcf6e2080013f76a6c730fc5b13cd3566e9ac
Torsten Paul <Torsten.Paul@gmx.de>
2006-08-13 15:58:57 +0200

    Handle both Robotron and Rossendorf FDC module ports

--- src/kc/fdc7.cc
+++ src/kc/fdc7.cc
@@ -49,6 +49,39 @@ FDC7::add_callback(unsigned long long offset, Callback *cb, void *data)
   z80->addCallback(offset, cb, data);
 }
 
+/*
+ *  Documentation from Volker...
+ *
+ *  ; FDC-Ports:
+ *  ; ----------
+ *  
+ *  	'ROBOTRON'
+ *  
+ *  FDCD         	equ	98h  	; FDC Datenregister
+ *  FDCC         	equ	99h  	; FDC Steueregister	
+ *  FDCZ        	equ	0A0h 	; FDC Zusatzregister
+ *  				; 5 4 3 2 1 0
+ *  				; x x 0 0 x x 
+ *  				; | |     | |
+ *  				; | |     | Motor Laufwerk 0 ein/aus
+ *  				; | |     Motor Laufwerk 1 ein/aus
+ *  				; | Terminal Count aktivieren/deakt.
+ *  				; FDC Reset
+ *  
+ *  	ELSE Rossendorf
+ *  
+ *  FDCD         	equ	10h  	; FDC Datenregister
+ *  FDCC         	equ	11h  	; FDC Steueregister	
+ *  FDCZ        	equ	12h 	; FDC Zusatzregister
+ *  				; 5 4 3 2 1 0
+ *  				; x x 0 0 x x 
+ *  				; | |     | |
+ *  				; | |     | Motor Laufwerk 0 ein/aus
+ *  				; | |     Motor Laufwerk 1 ein/aus
+ *  				; | Terminal Count aktivieren/deakt.
+ *  				; FDC Reset
+ * 
+ */
 byte_t
 FDC7::in(word_t addr)
 {
@@ -56,15 +89,16 @@ FDC7::in(word_t addr)
 
   switch (addr & 0xff)
     {
-    case 0x98: // CPM-Z9 module (Status Register)
-    case 0x10: // CPM-Z9 module (Status Register) ???
+    case 0x98: // CPM-Z9 module (Data Register, ROBOTRON module)
+    case 0x10: // CPM-Z9 module (Data Register, Rossendorf module)
       val = get_msr();
       break;
-    case 0x99: // CPM-Z9 module (Data Register)
-    case 0x11: // CPM-Z9 module (Data Register) ??
+    case 0x99: // CPM-Z9 module (Status Register, ROBOTRON module)
+    case 0x11: // CPM-Z9 module (Status Register, Rossendorf module)
       val = in_data(addr);
       break;
-    case 0xa0: // CPM-Z9 module
+    case 0xa0: // CPM-Z9 module (Extra Register, ROBOTRON module)
+    case 0x12: // CPM-Z9 module (Extra Register, Rossendorf module)
       break;
     default:
       DBG(2, form("KCemu/FDC/in_unhandled",
@@ -81,13 +115,16 @@ FDC7::out(word_t addr, byte_t val)
 {
   switch (addr & 0xff)
     {
-    case 0x99: // CPM-Z9 module (Status Register)
-      out_data(addr, val);
-      break;
-    case 0x98: // CPM-Z9 module (Data Register)
+    case 0x98: // CPM-Z9 module (Data Register, ROBOTRON module)
+    case 0x10: // CPM-Z9 module (Data Register, Rossendorf module)
       write_byte(val);
       break;
-    case 0xa0: // CPM-Z9 module
+    case 0x99: // CPM-Z9 module (Status Register, ROBOTRON module)
+    case 0x11: // CPM-Z9 module (Status Register, Rossendorf module)
+      out_data(addr, val);
+      break;
+    case 0xa0: // CPM-Z9 module (Extra Register, ROBOTRON module)
+    case 0x12: // CPM-Z9 module (Extra Register, Rossendorf module)
       set_terminal_count((val & 0x10) == 0x10);
 
       if ((val & 0x20) == 0x20)
