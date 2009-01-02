729638717f9c0aebd946f98329c4fc0498517b48
Torsten Paul <Torsten.Paul@gmx.de>
2009-01-02 20:28:48 +0100

    Change handling of the D004 terminal count signal
    The fix to the index hole pulse generation that changed
    the timing (commit 28b1233d1239af33baa1771ef7b050592b893958)
    seems to have only shifted the odds for the problem to
    show up.
    Further testing points to the terminal count signal that
    is sent after retrieving the data from the FDC. Up to
    now only the output value of 0xc0 did have any effect on
    the FDC state. Now every value that is not 0x00 will cause
    the FDC to enter the result state.

--- src/kc/fdc4.cc
+++ src/kc/fdc4.cc
@@ -155,7 +155,7 @@ FDC4::out(word_t addr, byte_t val)
       DBG(2, form("KCemu/FDC/out_F8",
                   "FDC::out(): %04xh TC %02x\n",
                   r.PC.D, val));
-      if (val == 0xc0) // kc85/4
+      if (val != 0x00) // kc85/4
 	set_state(FDC_STATE_RESULT);
 
       set_input_gate(0x40, 0x00);
